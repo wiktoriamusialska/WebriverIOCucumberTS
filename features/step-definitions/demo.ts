10ae5ac5a03e1698e945aa1212175ad84e269cae
wiktoriamusialska <wiktoria.musialska@interia.pl>
2025-03-16 13:59:33 +0100

    Dropdown

--- features/step-definitions/demo.ts
+++ features/step-definitions/demo.ts
@@ -1,12 +1,16 @@
 import { Given, When} from '@wdio/cucumber-framework';
-
 Given(/^A web page is opened$/,async function(){
-    await browser.url("https://the-internet.herokuapp.com/inputs");
+    //Inputbox
+    //await browser.url("https://the-internet.herokuapp.com/inputs");
+    //Dropdown
+    await browser.url("https://the-internet.herokuapp.com/dropdown");
     await browser.setTimeout({implicit:15000, pageLoad:1000});
     await browser.maximizeWindow();
 })
 
 When(/^Perform web interactions$/, async function () {
+    /*
+    //Inputbox
     let num=12345;
     let strNum = num.toString();
     let ele = await $('[type=number]');
@@ -18,5 +22,24 @@ When(/^Perform web interactions$/, async function () {
         await browser.pause(1000);
         await browser.keys(charStr);    
     }
-    //await browser.debug();
+    */
+
+    /*
+    //Dropdown - one option
+    let ddEle =await $('#dropdown');
+    //await ddEle.selectByVisibleText("Option 2")
+    //await ddEle.selectByAttribute("value","1");
+    await ddEle.selectByIndex(2); */
+
+    //Dropdown - list of option
+    let eleArr= await $$(`select > option`);
+    let arr= [];
+    for (let i=0; i<await eleArr.length;i++){
+        let ele=eleArr[i];
+        let val = await ele.getText();
+        arr.push(val);
+    }
+    console.log(`>>Option Array: |${arr}|`);
+    //await browser.debug(); 
+
 })
